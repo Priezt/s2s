188ee19143a9422c4d38b2619fd7ab1c62e8adbe
Priezt <vegetabird@gmail.com>
2020-05-26 12:03:19 +0800

    ...

--- s4s.py
+++ s4s.py
@@ -46,7 +46,7 @@ if len(args) == 0:
 COMMAND = " ".join(args)
 
 app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+#app.config["DEBUG"] = True
 
 @app.route('/', methods=[METHOD])
 def index():
